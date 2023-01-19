47cf29e4ed35db7e97f75c1873396db275973c2b
Melvin-Jovano <melvinjovano2@gmail.com>
2023-01-19 19:19:00 +0700

    chore: add chat UI

---
+++ src/components/chat/NewMessageModal.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="modal fade" id="new-message-modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                    <div class="modal-title">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>&emsp;
+                        <h5 class="fw-bold d-inline">
+                            New Message
+                        </h5>
+                    </div>
+                    <div>
+                        <button class="py-1 px-3 btn-primary btn btn-lg border-50px bg-primary-twitter text-sm fw-bold text-sm">
+                            Next
+                        </button>
+                    </div>
+                </div>
+
+                <div class="border-bottom">
+                    <div class="input-group">
+                        <span class="mx-3 my-2" id="basic-addon1"><IconSearch /></span>
+                        <input type="search" class="form-control form-control-sm border-0 outline-none outline-focus-none" placeholder="Search people">
+                    </div>
+                </div>
+
+                <div class="hv-65 overflow-auto">
+                    <div class="px-3 py-2 cursor-pointer profil-card">
+                        <table>
+                            <tr>
+                                <td rowspan="2" class="">
+                                    <img src="https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80" alt="" class="rounded-circle" width="40" height="40" srcset="">
+                                    &nbsp;&nbsp;
+                                </td>
+                                <td class="fw-bold text-sm">Joki Tugasmu</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">@joki</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import IconSearch from '../../assets/icons/IconSearch.vue';
+</script>
+
+<style scoped>
+    .profil-card:hover {
+        background-color: rgba(144, 178, 245, 0.15);
+    }
+</style>
---
+++ src/views/MessageView.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container justify-content-between p-4">
+        <div class="row">
+            <div class="col-3">
+                <HeaderSide />
+            </div>
+            <div class="col-3 hv-95 overflow-auto">
+                <div class="row px-2 mb-4">
+                    <div class="col-8">
+                        <h4>Messages</h4>
+                    </div>
+                    <div class="col-1">
+                        <IconChatSetting />
+                    </div>
+                    <div class="col-1">
+                        <IconChatNewChat />
+                    </div>
+                </div>
+                <div class="px-3">
+                    <h2 class="fw-bold">
+                        Welcome to your inbox!
+                    </h2>
+                    <p class="text-muted mb-4">
+                        Drop a line, share Tweets and more with private conversations between you and others on Twitter. 
+                    </p>
+                    <div>
+                        <button data-bs-toggle="modal" data-bs-target="#new-message-modal" class="p-3 px-4 btn-primary btn btn-lg border-50px bg-primary-twitter text-sm fw-bold text-sm">
+                            Write a message
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center overflow-auto">
+                <div class="w-50">
+                    <h3 class="fw-bold">Select a message</h3>
+                    <p class="text-muted px-2 mb-4">
+                        Choose from your existing conversations, start a new one, or just keep swimming.
+                    </p>
+                    <button data-bs-toggle="modal" data-bs-target="#new-message-modal" class="py-3 px-5 btn-primary btn btn-lg border-50px bg-primary-twitter text-sm fw-bold text-sm">
+                        New message
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <NewMessageModal />
+</template>
+
+<script setup>
+    import HeaderSide from '../components/home/HeaderSide.vue';
+    import IconChatSetting from '../assets/icons/IconChatSetting.vue';
+    import IconChatNewChat from '../assets/icons/IconChatNewChat.vue';
+    import NewMessageModal from '../components/chat/NewMessageModal.vue';
+</script>
