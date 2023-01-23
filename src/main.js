d527f296ddd696ae4e0225a7d05c5fc86a8f8eea
Melvin Jovano <57622164+Melvin-Jovano@users.noreply.github.com>
2023-01-24 01:42:17 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,55 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+
+let isRefresh = true;
+
+axios.interceptors.response.use(async response => {
+  const originalRequest = response.config;
+  
+  if (response.data.message === 'EXPIRED_TOKEN') {
+    const isRefreshNewInstance = isRefresh;
+
+    if (isRefreshNewInstance) {
+      isRefresh = false;
+      
+      const refreshUserToken = await refreshToken();
+      if(!refreshUserToken.success) {
+        await logout();
+        localStorage.clear();
+        router.go('/login');
+      }
+
+      localStorage.setItem('accessToken', refreshUserToken.data.accessToken);
+      localStorage.setItem('refreshToken', refreshUserToken.data.refreshToken);
+
+      isRefresh = true;
+      originalRequest.headers.Authorization = `Bearer ${refreshUserToken.data.accessToken}`;
+      return await axios.request(originalRequest);
+    } else {
+      return await axios.request(originalRequest);
+    }
+
+  } else if (response.data.message === 'INVALID_TOKEN') {
+    await logout();
+    localStorage.clear();
+    router.go('/login');
+  }
+
+  return response;
+});
+
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 const app = createApp(App)
 
