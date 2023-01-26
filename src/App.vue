d5db67907b9e68b914e8a5d5a0dac9b4ca8f67d5
Melvin-Jovano <melvinjovano2@gmail.com>
2023-01-27 06:49:26 +0700

    feat: finalizing auth

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template>
   <RouterView />
 </template>
+
+<script setup>
+  import { onMounted } from 'vue';
+  import app from './stores/app';
+
+  const appStores = app();
+
+  onMounted(() => {
+    document.body.addEventListener('click', (e) => {
+      try {
+        if(e.target.dataset.event === 'removeOptions') {
+          appStores.showOption = !appStores.showOption;
+          return;
+        }
+        throw Error();
+      } catch (error) {
+        appStores.showOption = false;
+      }
+    });
+  });
+</script>
